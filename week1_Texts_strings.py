786efd5cf400cff32f4d22cf5f049439837eb08b
Istvan Zsely <zselyigy@gmail.com>
2023-11-27 12:55:58 +0100

    task 16 done

--- week1_Texts_strings.py
+++ week1_Texts_strings.py
@@ -1,4 +1,4 @@
-a = 'Jack Bauer'
+a = 'Jack Bauer Jack'
 # convert a string to upper case
 print(a.upper())
 # convert a string to lower case
@@ -6,11 +6,19 @@ print(a.lower())
 
 
 
-a = 'jack Bauer'
+b = 'jack Bauer'
 # convert a string to title case
-print(a.title())
+print(b.title())
 
-a = ' \t aa    bb \f   '
-print(a)
+b = ' \t aa    bb \f   '
+print(b)
 # removes any leading, and trailing whitespaces 
-print(a.strip())
+c = b.strip()
+print()
+
+# replace a part of the strig with a new one, does it for each occurence
+a = a.replace('Jack', 'John')
+print(a)
+
+# replace double spcaes to single ones
+print(c.replace('  ', ' '))
