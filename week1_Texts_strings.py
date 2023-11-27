8e1e923e0deee5cca90e50043ed3105a75ecbb00
Istvan Zsely <zselyigy@gmail.com>
2023-11-27 12:13:46 +0100

    task 14 done

---
+++ week1_Texts_strings.py
@@ -0,0 +1,11 @@
+a = 'Jack Bauer'
+# convert a string to upper case
+print(a.upper())
+# convert a string to lower case
+print(a.lower())
+
+
+
+a = 'jack Bauer'
+# convert a string to title case
+print(a.title())
