465e388e7532d4b61196590c382c4486e4f34a75
Istvan Zsely <zselyigy@gmail.com>
2023-11-27 09:59:34 +0100

    task 006 done file renamed

---
+++ week1_Basics.py
@@ -0,0 +1,14 @@
+# this is a comment
+# we add 2 numbers
+a = 5
+b = 3
+print(a + b)
+
+a = 'Text1'
+b = "Text2"
+print(a + b)
+
+# use lower case for your variables, do not use space, but underline
+full_name = 'Jack'
+# this is camel case
+fullName = 'Jack'
