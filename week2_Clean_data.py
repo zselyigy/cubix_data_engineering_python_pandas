3f2293372ad1697b1ef32e6b0b1556e0dfed3d44
Istvan Zsely <zselyigy@gmail.com>
2023-11-30 20:01:57 +0100

    task 38 done

--- week2_Clean_data.py
+++ week2_Clean_data.py
@@ -32,9 +32,20 @@ df[['Day', 'Month', 'Year']] = df['Date'].str.split('.', expand = True)
 # print(df.dtypes)
 # df['Date'] = df['Date'].astype('datetime64[ns]')
 df['Date'] = pd.to_datetime(df['Date'], dayfirst = True)
-print(df)
 
 # conversion of a column to numeric
 # df['xxx'] = pd.to_numeric(df['xxx'], errors = 'coerce')
 # conversion of the data of a column to float
 # df['xxx'] = df['xxx'].astype(dtype = 'float64', errors = 'ignore')
+
+# delete the Description column
+df = df.drop(['Description'], axis = 1)
+# delete multiple columns, e.g. Day, Month and Year
+df = df.drop(['Day', 'Month', 'Year'], axis = 1)
+
+# deleta row(s)
+df2 = df.drop([1, 3, 4], axis = 0)
+
+# filtering by the content of some column
+df2 = df.drop(df.index[df['Product'] == '640 Fax Machine'])
+print(df2)
