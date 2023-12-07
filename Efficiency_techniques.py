2e7a584d7cfb6acb13c7c560d56fb0a80948d070
Istvan Zsely <zselyigy@gmail.com>
2023-12-07 17:02:19 +0100

    task 67 done

--- Efficiency_techniques.py
+++ Efficiency_techniques.py
@@ -21,8 +21,27 @@ try:
 except:
     print('We cannot divide by zero!')
 
-n = input('Type a number: ')
+# n = input('Type a number: ')
+n = 4
 try:
     print('The number squared: ', float(n) ** 2)
 except:
     print('Type a NUMBER, not a text!')
+
+import os, shutil
+basepath = r'.\Python_DA'
+subfolder = 'Test folder'
+# create a folder (if it does not exist)
+try:
+    os.mkdir(basepath + '\\' + subfolder)
+except:
+    pass   # in case of error (e.g. the subfolder exists) do nothing
+
+# # remove a folder (only if empty)
+# try:
+#     os.rmdir(basepath + '\\' + subfolder)
+# except:
+#     pass   # in case of error (e.g. the subfolder is not empty) do nothing
+
+# remove a folder with all files in
+shutil.rmtree(basepath + '\\' + subfolder, ignore_errors = True)
