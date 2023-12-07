c6ef4134898598814d5304c462aae61d50d612ff
Istvan Zsely <zselyigy@gmail.com>
2023-12-07 16:29:41 +0100

    task 64 done

---
+++ Efficiency_techniques.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+birth_date = dt.date(2020, 3, 21)
+print(birth_date)
+print(birth_date.strftime('%x'))    # local date
+print(birth_date.strftime('%Y'))    # year full
+print(birth_date.strftime('%m'))    # month number
+print(birth_date.strftime('%B'))    # month full name
+print(birth_date.strftime('%d'))    # day as number (1-31)
+print(birth_date.strftime('%A'))    # day full name
+print(birth_date.strftime('%w'))    # weekday as a number (0-6), 0 is SUnday
+
+today_date = dt.datetime.now()
+print(today_date)
+print(today_date.strftime('%x'))
