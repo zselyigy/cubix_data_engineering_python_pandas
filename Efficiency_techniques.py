c1605486dc652e1450b1e98db0f5b23d8af0382f
Istvan Zsely <zselyigy@gmail.com>
2023-12-07 16:47:24 +0100

    task 65 done

--- Efficiency_techniques.py
+++ Efficiency_techniques.py
@@ -13,3 +13,10 @@ print(birth_date.strftime('%w'))    # weekday as a number (0-6), 0 is SUnday
 today_date = dt.datetime.now()
 print(today_date)
 print(today_date.strftime('%x'))
+
+a = 6
+b = 0
+try:
+    print('The quotient is ' + str(a / b))
+except:
+    print('We cannot divide by zero!')
