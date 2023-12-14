858c2b306fb9caa4c3774cb908e0d7958ed1728c
Istvan Zsely <zselyigy@gmail.com>
2023-12-14 15:15:25 +0100

    task 97 done

--- read_parameters.py
+++ read_parameters.py
@@ -5,4 +5,8 @@ paramfile = 'Parameters.xlsx'
 paramfile_sheet = 'Parameter tables'
 
 df_param = pd.read_excel(paramfolderpath + '\\' + paramfile, sheet_name = paramfile_sheet, usecols = 'A:B')
+print(df_param) # all rows are read, but most of them do not contain information
+
+# drop the rows where there is no value in column 'Input file'
+df_param = df_param.dropna(subset= ['Input file'])
 print(df_param)
