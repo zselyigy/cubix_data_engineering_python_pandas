80fecbebd09aeebf40c800efce7ba26e2c55337b
Istvan Zsely <zselyigy@gmail.com>
2024-01-02 14:25:01 +0100

    task 109 done

--- Dictionaries.py
+++ Dictionaries.py
@@ -13,3 +13,19 @@ print(personal_data.values())
 
 # get all items (key - value pairs) of a dictonary
 print(personal_data.items())
+
+# modify a value for a key in a dictionary
+personal_data['Age'] = 30
+print(personal_data)
+
+# loop through the items of dictionary
+for i in personal_data:
+    print(i, '-', personal_data[i])
+
+# loop through the items of dictionary - an alternative way
+for key, value in personal_data.items():
+    print(key, '-', value)
+
+# add a new item (key-value pair) to the dictionary
+personal_data['Height'] = 180
+print(personal_data)
