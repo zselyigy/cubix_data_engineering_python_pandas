b54ca84938f6a7b1b0709a34f4119b784b40d22d
Istvan Zsely <zselyigy@gmail.com>
2024-01-02 14:16:56 +0100

    task 108 done

---
+++ Dictionaries.py
@@ -0,0 +1,15 @@
+# create a dictionary
+english_to_german = {'Year': 'Jahr', 'Month': 'Monat', 'Data': 'Datum'}
+personal_data = {'Name': 'Peter', 'Age': 36, 'Country': 'UK'}
+
+# value for the key in the dictionary
+print(personal_data['Age'])
+
+# get all the keys of a dictonary
+print(personal_data.keys())
+
+# get all the values of a dictonary
+print(personal_data.values())
+
+# get all items (key - value pairs) of a dictonary
+print(personal_data.items())
